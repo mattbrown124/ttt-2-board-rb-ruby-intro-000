f9f935ad4d05e75123de6e9ec50e51d6132a4c7f
Matt Brown <matthewtbrown124@gmail.com>
2021-02-03 19:46:09 +0000

    Automatically backed up by Learn

--- lib/board.rb
+++ lib/board.rb
@@ -1,1 +1,22 @@
 # Define the variable board below.
+board = [" ", " ", " "
+" ", " ", " "
+" ", " ", " "]
+# Position 1: left
+board[0]
+# Position 2: Middle
+board[1]
+# Poition 3: right
+board[2]
+# Position 4: left
+board[3]
+# Position 5: middle
+board[4]
+# Position 6: right
+board[5]
+# Position 7: left
+board[6]
+# Position 8: middle
+board[7]
+# Position 9: right
+board[8]
